77fd17a03ae060baeeea4a2cd377c2aa94766f0e
Nicole Lopez <nmlopez21@gmail.com>
2015-05-09 14:39:32 -0500

    initialize & init_transaction functions work

--- CreditAccount.rb
+++ CreditAccount.rb
@@ -2,10 +2,34 @@ require_relative "Transaction"
 
 class CreditAccount
 
-	attr_accessor :limit, :apr, :principal, :remaining_credit
+	attr_accessor :limit, :apr, :principal, :remaining_credit, :transactions
+
+	def initialize
+		@transactions = Array.new
+		@apr = 0.35
+		@principal = 0
+		puts "What's the limit on the account?"
+		@limit = gets.chomp.to_f
+		@remaining_credit = @limit
+	end
 
 	def get_principal
 		puts "This is the principal: $#{principal}"
+	end
+
+	def init_transaction
+		puts "Is this a payment or withdrawal?"
+		type = gets.chomp
+		if type == "withdrawal"
+			verb = "withdraw"
+		elsif type == "payment"
+			verb = "pay"
+		else
+			puts "Please type either 'payment' or 'withdrawal'."
+		end
+		puts "Great, you're making a #{type}."
+		puts "What amount would you like to #{verb}?"
+		amount = gets.chomp.to_f
 	end
 
 	def do_transaction(type, amount)
